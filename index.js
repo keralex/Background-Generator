eba0be30d1983e994a4cea95f530b411a5ba47d7
keralex <kerlis.aguado@correo.unimet.edu.ve>
2019-10-10 22:22:28 -0300

    almost finished

---
+++ index.js
@@ -0,0 +1,18 @@
+var css = document.querySelector("h3");
+var color_1 = document.querySelector(".color_1");
+var color_2 = document.querySelector(".color_2");
+var body = document.getElementById("gradient");
+
+
+// funtions
+function setGradient() {
+    body.style.background = "linear-gradient(to right,"
+        + color_1.value
+        + ","
+        + color_2.value
+        + ")";
+
+    css.textContent=body.style.background+";";
+}
+color_1.addEventListener("input", setGradient);
+color_2.addEventListener("input", setGradient)  ;
